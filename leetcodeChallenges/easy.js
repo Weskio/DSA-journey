d29fe183468c20c3081203c07b4c8c46a15398a2
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-03-06 23:56:41 +0000

    longest palindromic substring in s

--- leetcodeChallenges/easy.js
+++ leetcodeChallenges/easy.js
@@ -40,3 +40,27 @@ const twoSumOptimized = (nums, target) => {
 console.log("optimized two sum:", twoSumOptimized(nums, target));
 
 //PS: i feel dumb for feeling happy my solution worked😪
+
+
+//Given a string s, return the longest palindromic substring in s.
+const longestPalindrome = (s) => {
+  let longest = "";
+
+  function isPalindrome(str) {
+      return str === str.split("").reverse().join("");
+  }
+
+  for (let i = 0; i < s.length; i++) {
+      for (let j = i + 1; j <= s.length; j++) {
+          const substring = s.slice(i, j);
+          if (isPalindrome(substring) && substring.length > longest.length) {
+              longest = substring;
+          }
+      }
+  }
+
+  return longest;
+}
+
+console.log(longestPalindrome("babad")); 
+console.log(longestPalindrome("cbbd")); 
