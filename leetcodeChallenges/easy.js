540bcbe867dbbb08df93c1a7043aa7a1cb703e36
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-03-08 23:59:30 +0000

    longest palindrome optimal solution

--- leetcodeChallenges/easy.js
+++ leetcodeChallenges/easy.js
@@ -63,3 +63,25 @@ const longestPalindrome = (s) => {
 
 console.log(longestPalindrome("babad"));
 console.log(longestPalindrome("cbbd"));
+
+const longestPalindrome2 = (s) => {
+  let longest = "";
+
+  const expandAroundCenter = (left, right) => {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return s.slice(left + 1, right);
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    const palindrome1 = expandAroundCenter(i, i);
+    const palindrome2 = expandAroundCenter(i, i + 1);
+
+    if (palindrome1.length > longest.length) longest = palindrome1;
+    if (palindrome2.length > longest.length) longest = palindrome2;
+  }
+
+  return longest;
+};
