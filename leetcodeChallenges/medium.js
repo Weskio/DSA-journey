e631986ad8fec4d2d122a56cbe2acdbe560377c7
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-03-17 23:59:12 +0000

    length of longest substring

--- leetcodeChallenges/medium.js
+++ leetcodeChallenges/medium.js
@@ -68,3 +68,25 @@ const jsonFixer = {
     return result;
   },
 };
+
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+// sliding window technique
+const lengthOfLongestSubstring = (s) => {
+  const charSet = new Set();
+  let left = 0; 
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+      while (charSet.has(s[right])) {
+          charSet.delete(s[left]);
+          left++;
+      }
+      charSet.add(s[right]);
+      maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
