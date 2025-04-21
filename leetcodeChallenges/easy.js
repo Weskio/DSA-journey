6d240a73c93c0d283a46795e4ce6fe77bd34e283
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-04-21 23:39:47 +0000

    move romanToInt to easy file

--- leetcodeChallenges/easy.js
+++ leetcodeChallenges/easy.js
@@ -96,5 +96,36 @@ console.log(longestPalindrome2("cbbd"));
 const isPalindrome = function (x) {
   let xStr = x.toString().split("").reverse().join("");
 
-  return (parseInt(xStr) === x)
+  return parseInt(xStr) === x;
 };
+
+// Roman to Int
+const romanToInt = (s) => {
+  const romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const current = romanMap[s[i]];
+    const next = romanMap[s[i + 1]];
+
+    if (current < next) {
+      total += next - current;
+      i++;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
+};
+
+console.log(romanToInt("IV"));
--- leetcodeChallenges/medium.js
+++ leetcodeChallenges/medium.js
@@ -201,34 +201,3 @@ const intToRoman = (num) => {
 };
 
 console.log(intToRoman(9));
-
-// Roman to Int
-const romanToInt = (s) => {
-  const romanMap = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
-
-  let total = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    const current = romanMap[s[i]];
-    const next = romanMap[s[i + 1]];
-
-    if (current < next) {
-      total += next - current;
-      i++;
-    } else {
-      total += current;
-    }
-  }
-
-  return total;
-};
-
-console.log(romanToInt('IV'))
