4d22abe494b50694d77cff6cab63929da269591a
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-04-21 23:44:05 +0000

    longest common prefix algo

--- leetcodeChallenges/easy.js
+++ leetcodeChallenges/easy.js
@@ -129,3 +129,23 @@ const romanToInt = (s) => {
 };
 
 console.log(romanToInt("IV"));
+
+
+// Longest Prefix
+const longestCommonPrefix = (strs) => {
+  if (!strs.length) return "";
+
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, -1);
+      if (!prefix) return "";
+    }
+  }
+
+  return prefix;
+}
+
+console.log(longestCommonPrefix(['flowers', 'flow', 'flood']))
+console.log(longestCommonPrefix(['flowers', 'flow', 'mop']))
