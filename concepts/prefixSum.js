6db36b09956b061998eb0356350aeb886b1688dd
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-05-24 21:04:43 +0000

    subarray sum equals k

--- concepts/prefixSum.js
+++ concepts/prefixSum.js
@@ -14,3 +14,25 @@ const arr = [10, 20, 10, 5, 15];
 console.log(computePrefixSum(arr));
 
 // more use cases for this algo concept to follow
+
+
+// subarray sum Equals K
+const subarraySum = (nums, k) => {
+  const prefixSumMap = { 0: 1 }; 
+  let currentSum = 0;
+  let count = 0;
+
+  for (const num of nums) {
+      currentSum += num; 
+
+      if (prefixSumMap[currentSum - k]) {
+          count += prefixSumMap[currentSum - k];
+      }
+
+      prefixSumMap[currentSum] = (prefixSumMap[currentSum] || 0) + 1;
+  }
+
+  return count;
+}
+
+console.log(subarraySum([1, 1, 1], 2));
